972b7ff59bf2b4b6176692a1034beec627cb5495
auffenberg <92852408+auffenberg@users.noreply.github.com>
2024-09-27 16:21:37 +0200

    not loading all datasets at once

--- main.py
+++ main.py
@@ -8,22 +8,21 @@ import pickle
 def classification(working_dir, n_classes, n_runs):
     RBF_A = 0.1
     RBF_B = 0.1
-    K = 3
-    datasets = [[my_datasets.load_caltech_data(working_dir, n_classes), 'caltech256' + str(i)] for i in range(n_runs)]
     results = {}
-    iter_count = 0
-    for (partitioned_data, partitioned_labels), dataset_name in datasets:
-        iter_count += 1
-        #print(f'Iteration: {iter_count}')
+
+    for i in range(n_runs):
+        partitioned_data, partitioned_labels = my_datasets.load_caltech_data(working_dir, n_classes)
+        dataset_name = 'caltech256_' + str(i)
+
         start_time = time.time()
+        print(f'Iteration count: {i + 1}')
         results[dataset_name] = my_testing.run_methods_on_dataset(partitioned_data=partitioned_data, partitioned_labels=partitioned_labels, RBF_A=RBF_A, RBF_B=RBF_B)
-        #print(f'Time: {time.time() - start_time}s')
+        print(f'Time: {time.time() - start_time}s')
     
     return results
 
 working_dir = Path(__file__).parent / 'datasets/Caltech256/caltech256/256_ObjectCategories'
 n_runs = 100
-
 
 binary_results = classification(working_dir=working_dir, n_classes=2, n_runs=n_runs)
 multiclass_results = classification(working_dir=working_dir, n_classes=4, n_runs=n_runs)
